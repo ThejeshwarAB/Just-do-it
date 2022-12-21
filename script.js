5cbdd1b74c7043c961e70057a277739084b383c7
ThejeshwarAB <abibastheja@gmail.com>
2022-12-21 23:21:39 +0530

    Added comments

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const todo = document.querySelector('.todo'); 
-const form = document.querySelector('.form');
-const info = document.querySelector('.info');
-const header = document.querySelector('.header');
-const filter = document.querySelector('.filter');
-const error = document.querySelector('.error');
-const tasks = todo.getElementsByTagName('li');
-const filtered = todo.getElementsByClassName('filtered');
+const todo = document.querySelector('.todo'); // unordered todo list 
+const form = document.querySelector('.form'); // input box to add new task
+const info = document.querySelector('.info'); // when there are no tasks
+const header = document.querySelector('.header'); // when there are one or more tasks
+const filter = document.querySelector('.filter'); // input box to enter search string
+const error = document.querySelector('.error'); // when the filter is not found
+const tasks = todo.getElementsByTagName('li'); // list of all the tasks
+const filtered = todo.getElementsByClassName('filtered'); // list of all filtered tasks
 
+// checks and updates the todo list
 const checkTasks = () => {
     if (tasks.length > 0) {
         info.classList.add('d-none');
@@ -20,36 +21,37 @@ const checkTasks = () => {
     }
 }
 
+// filters tasks based on the search string
 const filterTasks = (term) => {
 
     Array.from(todo.children)
         .filter((task) => !task.textContent.includes(term))
         .forEach((task) => task.classList.add('d-none', 'filtered'))
-    // .forEach((task)=>task.classList.add('filtered'))
 
     Array.from(todo.children)
         .filter((task) => task.textContent.includes(term))
         .forEach((task) => task.classList.remove('d-none', 'filtered'))
-    // .forEach((task)=>task.classList.remove('filtered'))
 }
 
 todo.addEventListener('click', e => {
 
-    if (e.target.classList.contains('fa-trash-can')) {
+    // when new task is added
+    if (e.target.classList.contains('fa-trash-can')) { 
         e.target.parentNode.parentNode.classList.add('outgoing');
+
+        // red color for outgoing tasks
         setTimeout(() => {
             e.target.parentNode.parentNode.remove();
             checkTasks();
         }, 1000);
-        // e.target.parentNode.parentNode.remove();
     }
-
+    // when task is completed
     else if (e.target.classList.contains('fa-circle-check')) {
         e.target.parentNode.parentNode.classList.add('completed');
         e.target.classList.remove('fa-circle-check');
         e.target.classList.add('fa-arrows-rotate');
     }
-
+    // when task progress is reset
     else if (e.target.classList.contains('fa-arrows-rotate')) {
         e.target.parentNode.parentNode.classList.remove('completed');
         e.target.classList.remove('fa-arrows-rotate');
@@ -59,15 +61,16 @@ todo.addEventListener('click', e => {
 
 form.addEventListener('submit', e => {
 
-    e.preventDefault();
+    e.preventDefault(); // prevents page reload
 
     var templateString = "";
 
+    // new task added as template string to todo list
     if (form.task.value != '') {
         templateString = `
         <li class="list-group-item d-flex justify-content-between align-items-center incoming">
                     <div class="task">
-                        ${form.task.value.toLowerCase()}
+                        ${form.task.value.toLowerCase().trim()}
                     </div>
                     <div class="buttons">
                         <i class="fa-sharp fa-solid fa-circle-check"></i>
@@ -79,28 +82,30 @@ form.addEventListener('submit', e => {
     }
 
     todo.innerHTML += templateString;
+
+    // green color for incoming tasks
     setTimeout(() => {
-        // console.log(todo.children)
         Array.from(tasks)
         .forEach((item)=>{
-            // console.log(item)
             item.classList.remove('incoming')})
     }, 1000);
+
     form.reset();
     checkTasks();
 })
 
 filter.addEventListener('keyup', e => {
-    // console.log(filter.value.trim())
-    const term = filter.value.trim();
-    filterTasks(term);
-    console.log("filtered",filtered.length)
-    console.log("tasks",tasks.length)
 
+    const term = filter.value.trim();
+    // filters tasks based on search string
+    filterTasks(term); 
+
+    // if no match is found
     if(filtered.length === tasks.length){
     error.classList.remove('d-none');
     header.classList.add('d-none');
     }
+    // if one or more match is found
     else{
     error.classList.add('d-none');
     header.classList.remove('d-none');
